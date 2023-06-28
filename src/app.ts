642724f5656b403cf27b76a8aac9281dd9947a60
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-28 16:30:42 -0500

    add to port an const

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(cors())
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(cors());
