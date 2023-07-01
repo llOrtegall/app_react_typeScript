cc1386433dbc58415a298ef86b57796f9bb1f54a
llOrtegall <IvanOrtega_97@hotmail.com>
2023-07-01 15:24:59 -0500

    descanso

--- src/App.ts
+++ src/App.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
-import { router } from "./routes/item";
+import { router } from "./routes";
 
 const PORT = process.env.PORT || 3001;
 const App = express();
---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { readdirSync } from "fs";
+
+const PATH_ROUTER = `${__dirname}`;
+const router = Router();
+
+const cleanFileName = (fileName: string) => {
+  const file = fileName.split('.').shift();
+  return file;
+}
+
+readdirSync(PATH_ROUTER).filter((fileName) => {
+
+  console.log(cleanFileName(fileName))
+})
+
+
+export { router }
