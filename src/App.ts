e2499b2c5aa1f42e1cd53896b44e0034f371195f
llOrtegall <IvanOrtega_97@hotmail.com>
2023-07-02 20:23:23 -0500

    conecxion a mongodb lista

--- src/App.ts
+++ src/App.ts
@@ -3,10 +3,12 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import db from "./config/mongo";
 
 const PORT = process.env.PORT || 3001;
 const App = express();
 
 App.use(cors());
 App.use(router);
+db().then(() => console.log("Conecxión Realizada"));
 App.listen(PORT, () => console.log(`Iniciando por el puerto ${PORT}`))
