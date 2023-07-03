09f7b97c32a26a57d7e63c3297214258401fcf41
llOrtegall <IvanOrtega_97@hotmail.com>
2023-07-02 20:28:11 -0500

    creando controlers

---
+++ src/controllers/item.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express"
+
+const getItem = (req: Request, res: Response) => {
+  try {
+
+  } catch (e) {
+
+  }
+}
+
+const getItems = (req: Request, res: Response) => {
+
+}
+
+const updateItem = (req: Request, res: Response) => {
+
+}
+
+const postItem = (req: Request, res: Response) => {
+
+}
+
+const deleteItem = (req: Request, res: Response) => {
+
+}
+
+
+
+export { getItem, getItems, postItem, deleteItem, updateItem };
