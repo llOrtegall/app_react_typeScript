fa5060a8fcc547daee3908aab491968694ec0eee
llOrtegall <IvanOrtega_97@hotmail.com>
2023-07-03 21:08:47 -0500

    schema ok

---
+++ src/models/item.ts
@@ -0,0 +1,31 @@
+import { Schema, Types, model, Model } from "mongoose";
+import { Car } from "../interfaces/car.interface";
+
+const ItemSchema = new Schema<Car>(
+  {
+    color: {
+      type: String,
+    },
+    gas: {
+      type: String,
+      enum: ["gasoline", "electric"],
+    },
+    year: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+const ItemModel = model('items', ItemSchema);
+
+export default ItemModel
