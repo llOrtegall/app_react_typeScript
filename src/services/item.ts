86c28c04f66a74aa63abe8cdb6514bd891d2aa33
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-10 22:42:50 -0500

    desde 0

--- src/services/item.ts
+++
@@ -1,9 +0,0 @@
-import { Car } from "../interfaces/car.interface";
-import ItemModel from "../models/item";
-
-const insertItem = async (item: Car) => {
-  const responseInsert = await ItemModel.create(item);
-  return responseInsert;
-}
-
-export { insertItem };
