7a39558915cb9d78d39630ce27c941e3e65e6321
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-11 00:18:05 -0500

    guardando en la base de datos ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,25 @@
-export const register = (req, res) => {
-  console.log(req.body);
-  res.send('Registrando...');
+import User from '../models/user.model.js';
+
+export const register = async (req, res) => {
+  const { email, password, username } = req.body;
+
+  try {
+    //TODO: Crea el usuario pero aun no lo guarda en la bd
+    const newUser = new User({
+      username,
+      email,
+      password
+    })
+
+    //TODO: Guardan el usuario en la bd
+    await newUser.save()
+    res.send('Registrando...');
+
+  } catch (error) {
+    console.log(error.message)
+  }
+
+
 }
 
 export const login = (req, res) => {
