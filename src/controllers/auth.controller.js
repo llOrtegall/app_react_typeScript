3dce8571997bdc11ec9a1240a46df6212465a959
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-11 00:37:38 -0500

    usuario creado

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,30 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
     //TODO: Crea el usuario pero aun no lo guarda en la bd
+
+    // * encrpytando la contraseña del usuario
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
       username,
       email,
-      password
-    })
+      password: passwordHash
+    });
 
     //TODO: Guardan el usuario en la bd
-    await newUser.save()
-    res.send('Registrando...');
+    const userSaved = await newUser.save()
+    res.json({
+      id: userSaved.id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt
+    });
 
   } catch (error) {
     console.log(error.message)
