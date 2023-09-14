957a43ed08ea8869291274e3064798214b4f061a
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-13 21:29:14 -0500

    test ok realizado

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/task')
+    await mongoose.connect('mongodb://127.0.0.1:27017/tasks')
     console.log('db is connected')
   } catch (error) {
     console.log(error)
