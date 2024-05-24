ae4f6246970d26b574f1fae73514e71df29edc08
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-05-24 13:59:19 -0500

    change name

--- src/config.js
+++
@@ -1,1 +0,0 @@
-export const TOKEN_SECRET = 'abwdouamwijub1235ondoabw1o24n145'
--- src/index.js
+++
@@ -1,7 +0,0 @@
-// TODO: Encargado de iniciar todos los servicios
-import app from './App.js'
-import { connectDB } from './db.js'
-
-connectDB();
-app.listen(3000);
-console.log('Server listening on port 3000')
